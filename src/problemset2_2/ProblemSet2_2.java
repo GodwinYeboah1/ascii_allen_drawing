4552d7bfc251928ef550560385cc7aefea8dfc2f
GodwinYeboah <godwinyeboah95@gmail.com>
2019-03-24 10:46:12 -0400

    first commit

---
+++ src/problemset2_2/ProblemSet2_2.java
@@ -0,0 +1,49 @@
+package problemset2_2;
+
+public class ProblemSet2_2 {
+	
+	public static void drawEar() {
+		 	// ear section	 
+	   	   	System.out.println("       /\\           /\\    "  );
+	   	   	System.out.println("                             " );
+	   	   	System.out.println("                              ");
+	   	 	System.out.println("                              ");
+	   	 	System.out.println("                              ");
+	   	 	System.out.println("                              ");
+	   	 	System.out.println(" \\______________________________/		");
+	   	   	 
+	}
+	 public static void drawHead() {
+		 	// head section	 
+	   	   	System.out.println(" |                             |    ");
+	   	   	System.out.println(" |      $               $      |    ");
+	   	   	System.out.println(" |                             |    ");
+	   	 	System.out.println(" |              *              |    ");
+	   	 	System.out.println(" |                             |	");
+	   	 	System.out.println(" \\              -              /     ");
+	   	 	System.out.println("   \\_______________________/		");
+	   	   	 
+	 }
+	
+	
+	 public static void drawLeg() {
+		// leg section
+   	   	System.out.println("    ||        ||        ||  ");
+   	   	System.out.println("    ||        ||        ||   ");
+   	   	System.out.println("   (||)      (||)      (||) ");
+   	   	
+     }
+	 
+
+	 
+	 
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+    	drawEar();
+    	drawHead();
+        drawLeg();
+ 
+        
+    } 	
+}
